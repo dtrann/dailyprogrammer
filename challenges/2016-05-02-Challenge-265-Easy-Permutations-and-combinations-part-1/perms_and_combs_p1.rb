149dbabb50f4af49f1bd5aa9af09a9417bb089d3
Damon Tran <damontran_@hotmail.com>
2016-05-28 03:30:04 -0600

    Update perms_and_combs_p1.rb

--- challenges/2016-05-02-Challenge-265-Easy-Permutations-and-combinations-part-1/perms_and_combs_p1.rb
+++ challenges/2016-05-02-Challenge-265-Easy-Permutations-and-combinations-part-1/perms_and_combs_p1.rb
@@ -1,2 +1,2 @@
-[*0...6].permutation.to_a.at(239).join(" ")
-[*0...7].permutation.to_a.at(3239).join(" ")
+puts [*0...6].permutation.to_a.at(239).join(" ")
+puts [*0...7].permutation.to_a.at(3239).join(" ")
