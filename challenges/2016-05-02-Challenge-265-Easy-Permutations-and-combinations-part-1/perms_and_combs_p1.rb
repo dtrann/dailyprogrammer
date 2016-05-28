1adb2654ba5159349480336bc92559ae33bb99b3
Damon Tran <damontran_@hotmail.com>
2016-05-28 03:55:26 -0600

    Finished solution for both parts, added comments

--- challenges/2016-05-02-Challenge-265-Easy-Permutations-and-combinations-part-1/perms_and_combs_p1.rb
+++ challenges/2016-05-02-Challenge-265-Easy-Permutations-and-combinations-part-1/perms_and_combs_p1.rb
@@ -1,2 +1,25 @@
-puts [*0...6].permutation.to_a.at(239).join(" ")
-puts [*0...7].permutation.to_a.at(3239).join(" ")
+=begin 
+Ruby practice
+
+COMMENTS WITH LINKS FOR FUTURE REFERENCE
+
+'puts' used to print out solution to console
+
+.permutation http://ruby-doc.org/core-2.2.0/Array.html#method-i-permutation
+.combination http://ruby-doc.org/core-2.2.0/Array.html#method-i-combination
+
+.to_a http://ruby-doc.org/core-2.2.0/Array.html#method-i-to_a
+converts to array
+
+.at(x) index at x, we used -1 because index 0
+
+.join http://ruby-doc.org/core-2.2.0/Array.html#method-i-join
+string together the elements of the array on " "
+
+=end
+
+puts [*0...6].permutation.to_a.at(240-1).join(" ")
+puts [*0...7].permutation.to_a.at(3240-1).join(" ")
+
+puts [*0...8].combination(3).to_a.at(24-1).join(" ")
+puts [*0...9].combination(4).to_a.at(112-1).join(" ")
